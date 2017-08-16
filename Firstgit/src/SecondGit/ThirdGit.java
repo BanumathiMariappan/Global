30de66d12c52ddbda43fe0d912ddaf0fc2ba53dc
alagupandi <alagupandi@fourbends.com>
2017-08-16 12:45:18 +0530

    Initial Change in my project

--- Firstgit/src/SecondGit/ThirdGit.java
+++ Firstgit/src/SecondGit/ThirdGit.java
@@ -5,7 +5,9 @@ public class ThirdGit {
 		
 			public static void main(String [] args){
 				System.out.println("Good Morning");
+				System.out.println("Good Afternoon");
 				System.out.println("Good Evening");
+				System.out.println("Good Night");
 			}
 
 		}
