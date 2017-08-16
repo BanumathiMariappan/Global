f7e71a8141577769806a14c64b96ed98864d0eed
Banumathi <banumathi.m@fourbends.com>
2017-08-16 17:57:26 +0530

    First process in global processing

---
+++ BB11/src/BB12/BB13.java
@@ -0,0 +1,9 @@
+package BB12;
+
+public class BB13 {
+	public static void main(String [] args){
+	System.out.println("Good Morning");
+	System.out.println("Have a nice Day");
+	System.out.println("Happy Life");
+	
+}}
