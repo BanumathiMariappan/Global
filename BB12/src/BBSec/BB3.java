590a8702a7f46c8d7434bcd9fe2ac4a8362eb74a
Banumathi <banumathi.m@fourbends.com>
2017-08-16 17:34:52 +0530

    Bit bucket First Commit in global

---
+++ BB12/src/BBSec/BB3.java
@@ -0,0 +1,10 @@
+package BBSec;
+
+public class BB3 {
+	public static void main(String [] args){
+		System.out.println("Good Morning");
+		System.out.println("Good Afternoon");
+		System.out.println("Good Evening");
+		}
+
+}
