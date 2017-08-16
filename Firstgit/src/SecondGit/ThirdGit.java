36d744416447ca2907386a3e82ecdf178abbecea
Banumathi <banumathi.m@fourbends.com>
2017-08-16 11:58:30 +0530

    Initial Process

---
+++ Firstgit/src/SecondGit/ThirdGit.java
@@ -0,0 +1,14 @@
+package SecondGit;
+
+public class ThirdGit {
+	
+		
+			public static void main(String [] args){
+				System.out.println("Good Morning");
+				System.out.println("Good Evening");
+			}
+
+		}
+
+
+
